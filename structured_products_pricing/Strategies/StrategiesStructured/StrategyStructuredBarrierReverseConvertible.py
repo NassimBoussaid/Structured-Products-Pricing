f5f80aea18fe7d7f8b7e0541580a49163c6f5ccd
nnayr <nassim.boussaid@dauphine.eu>
2025-04-21 11:21:39 +0200

    RC with coupons

--- structured_products_pricing/Strategies/StrategiesStructured/StrategyStructuredBarrierReverseConvertible.py
+++ structured_products_pricing/Strategies/StrategiesStructured/StrategyStructuredBarrierReverseConvertible.py
@@ -3,14 +3,18 @@ from structured_products_pricing.Parameters.Option.OptionBarrier import OptionBa
 from structured_products_pricing.Parameters.Pricer.PricerBase import PricerBase
 from structured_products_pricing.Products.Options.OptionPricerManager import OptionPricerManager
 from structured_products_pricing.Strategies.StrategiesCertificates.StrategyCertificateBase import StrategyCertificateBase
+from structured_products_pricing.Strategies.StrategiesOption.StrategyDigitalReplication import StrategyDigitalReplication
 from datetime import datetime
 
 class StrategyStructuredBarrierReverseConvertible(StrategyCertificateBase):
     def __init__(self, MarketObject: Market, PricerObject: PricerBase, strike: float, barrier_level: float,
-                 maturity_date: datetime):
+                 coupon_level: float, maturity_date: datetime):
         super().__init__(MarketObject, PricerObject, maturity_date)
         self.strategy_name = "Barrier Reverse Convertible"
-        option = OptionBarrier("Put", strike, maturity_date, "in", "down", barrier_level, "American")
-        option_params = OptionPricerManager(self.Market, option, self.Pricer)
-        self.products_params += [option_params]
-        self.quantities += [-1]
+        option_1 = OptionBarrier("Put", strike, maturity_date, "in", "down", barrier_level, "American")
+        option_2 = StrategyDigitalReplication(self.Market, self.Pricer, "Call", 0, 0.01, maturity_date)
+        option_1_params = OptionPricerManager(self.Market, option_1, self.Pricer)
+        option_2_params = OptionPricerManager(self.Market, option_2, self.Pricer)
+        coupon_gearing = coupon_level * 100
+        self.products_params += [option_1_params, option_2_params]
+        self.quantities += [-1, coupon_gearing]
--- structured_products_pricing/Strategies/StrategiesStructured/StrategyStructuredReverseConvertible.py
+++ structured_products_pricing/Strategies/StrategiesStructured/StrategyStructuredReverseConvertible.py
@@ -3,13 +3,18 @@ from structured_products_pricing.Parameters.Option.OptionEuropean import OptionE
 from structured_products_pricing.Parameters.Pricer.PricerBase import PricerBase
 from structured_products_pricing.Products.Options.OptionPricerManager import OptionPricerManager
 from structured_products_pricing.Strategies.StrategiesCertificates.StrategyCertificateBase import StrategyCertificateBase
+from structured_products_pricing.Strategies.StrategiesOption.StrategyDigitalReplication import StrategyDigitalReplication
 from datetime import datetime
 
 class StrategyStructuredReverseConvertible(StrategyCertificateBase):
-    def __init__(self, MarketObject: Market, PricerObject: PricerBase, strike: float, maturity_date: datetime):
+    def __init__(self, MarketObject: Market, PricerObject: PricerBase, strike: float, coupon_level: float,
+                 maturity_date: datetime):
         super().__init__(MarketObject, PricerObject, maturity_date)
         self.strategy_name = "Reverse Convertible"
-        option = OptionEuropean("Put", strike, maturity_date)
-        option_params = OptionPricerManager(self.Market, option, self.Pricer)
-        self.products_params += [option_params]
-        self.quantities += [-1]
+        option_1 = OptionEuropean("Put", strike, maturity_date)
+        option_2 = StrategyDigitalReplication(self.Market, self.Pricer, "Call", 0, 0.01, maturity_date)
+        option_1_params = OptionPricerManager(self.Market, option_1, self.Pricer)
+        option_2_params = OptionPricerManager(self.Market, option_2, self.Pricer)
+        coupon_gearing = coupon_level * 100
+        self.products_params += [option_1_params, option_2_params]
+        self.quantities += [-1, coupon_gearing]
