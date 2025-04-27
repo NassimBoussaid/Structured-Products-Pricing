b5063b21f7422e250bc46b9240eac2ff69be4a6f
mourougayakarthy <karthymrg@gmail.com>
2025-04-27 23:30:50 +0200

    Update barrier_option.py

--- structured_products_pricing/views/barrier_option.py
+++ structured_products_pricing/views/barrier_option.py
@@ -74,8 +74,8 @@ def run():
         maturity_date = st.date_input("Maturity date:", value=datetime(2025, 1, 1))
 
         st.subheader("Monte Carlo Parameters")
-        n_steps = st.number_input("Number of time steps:", min_value=1, value=100)
-        n_draws = st.number_input("Number of paths:", min_value=1000, value=20000, step=1000)
+        n_steps = st.number_input("Number of time steps:", min_value=1, value=50)
+        n_draws = st.number_input("Number of paths:", min_value=1000, value=10000, step=1000)
         seed = st.number_input("Random seed:", min_value=0, value=1, step=1)
 
         if barrier_type != st.session_state.get("priced_barrier_type", None):
